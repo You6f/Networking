477fd60c6ec8405c05c2b1ab541ea8be973ebf27
yousif <yousifjijji2@gmail.com>
2024-03-07 10:27:11 +0100

    update du pingPong

--- Sender/main.cpp
+++ Sender/main.cpp
@@ -24,7 +24,7 @@ int main()
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "192.168.1.78", &serv_addr.sin_addr) <= 0)
+    if (inet_pton(AF_INET, "192.168.30.169", &serv_addr.sin_addr) <= 0)
     {
         std::cerr << "Invalid address/ Address not supported" << std::endl;
         return -1;
--- pingPongTest/main.cpp
+++ pingPongTest/main.cpp
@@ -2,16 +2,38 @@
 #include "readAndWrite.cpp"
 #include "reciever.cpp"
 #include "sender.cpp"
+#include <string>
 
 int main()
 {
-    recieve();
-    string texte;
-    cout << "veiller entrer une cible";
-    cin >> texte;
+    cout << "are you the host (yes/NO)";
+    string reponse;
+    if (reponse == "yes")
+    {
+        string hasard;
+        cout << "ok vous être sender";
+        recieve();
+        string texte;
+        cout << "veiller entrer une cible";
+        cin >> texte;
 
-    write(texte, 66, "data.json");
-    send();
+        write(texte, 66, "data.json");
+        send();
+        cout << "appuyer sur une touche";
+        cin >> hasard;
+    }
+    else
+    {
+        string texte;
+        string hasard;
+        cout << "veiller entrer une cible";
+        cin >> texte;
+        write(texte, 66, "data.json");
+        send();
+        cout << "appuyer sur une touche";
+        cin >> hasard;
+        recieve();
+    }
 
     // recieve();
 
